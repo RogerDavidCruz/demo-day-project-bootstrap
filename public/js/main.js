3eef634550ba17f044cb19d372836c2347cacf5f
Roger David Cruz <roger.david.cruz@gmail.com>
2019-08-22 00:05:16 -0400

    all style updates

--- public/js/main.js
+++ public/js/main.js
@@ -97,13 +97,13 @@ $(function() {
 
         if(bodyScroll > 100){
             navbar.addClass("nav-scroll");
-            logo.attr('src', 'img/logo-dark.png');
-            darkbg.attr('src', 'img/logo-light.png');
+            logo.attr('src', '/img/logo-dark.png');
+            darkbg.attr('src', '/img/logo-light.png');
 
         }else{
             navbar.removeClass("nav-scroll");
-            logo.attr('src', 'img/logo-light.png');
-            navbloglogo.attr('src', 'img/logo-dark.png');
+            logo.attr('src', '/img/logo-light.png');
+            navbloglogo.attr('src', '/img/logo-dark.png');
         }
     });
 
